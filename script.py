4f4be73ca3007a07978e42e489739e9170b719bc
06kaylee <06.kaylee@gmail.com>
2020-06-15 07:29:16 -0700

    Add parser and main function

--- script.py
+++ script.py
@@ -12,18 +12,20 @@ from email.mime.text import MIMEText
 import getpass
 import schedule
 
-parser = argparse.ArgumentParser(description="Find jobs at a specified location")
-parser.add_argument("Job", help="Enter the job you are looking for. Example: Nursing")
-parser.add_argument("City", help = "Enter the city. Example: Seattle")
-parser.add_argument("StateAbbrev", help = "Enter the state abbreviation. Example: WA")
-parser.add_argument("Email", help = "Enter your gmail address. Example: test123@gmail.com")
-args = parser.parse_args()
 
-job = args.Job
-city = args.City
-state = args.StateAbbrev
-sender_email = args.Email
-receiver_email = args.Email
+def get_args():
+    parser = argparse.ArgumentParser(description="Find jobs at a specified location")
+    parser.add_argument("Job", help="Enter the job you are looking for. Example: Nursing")
+    parser.add_argument("City", help = "Enter the city. Example: Seattle")
+    parser.add_argument("StateAbbrev", help = "Enter the state abbreviation. Example: WA")
+    parser.add_argument("Email", help = "Enter your gmail address. Example: test123@gmail.com")
+    args = parser.parse_args()
+
+    job = args.Job
+    city = args.City
+    state = args.StateAbbrev
+    email = args.Email
+    return [job, city, state, email]
 
 
 def job_title(soup):
@@ -33,12 +35,14 @@ def job_title(soup):
             jobs.append(h2.a.text.strip())
     return jobs
 
+
 def job_link(soup):
     links = []
     for section in soup.find_all(name = "section", attrs = {"class": "card-content"}):
         for h2 in section.find_all(name = "h2", attrs = {"class": "title"}):
             links.append(h2.a.get("href"))
     return links
+
 
 def job_company(soup):
     companies = []
@@ -57,7 +61,8 @@ def job_location(soup):
                 locations.append(span.text.strip())
     return locations
 
-def set_up():
+
+def set_up(job, city, state):
     url = f'https://www.monster.com/jobs/search/?q={job}&where={city}__2C-{state}'
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
@@ -70,16 +75,11 @@ def get_info(soup):
     companies = job_company(soup)
     locations = job_location(soup)
     links = job_link(soup)
-    df = pd.DataFrame(list(zip(titles, companies, locations, links)), columns = columns)
-    return df
+    results_dataframe = pd.DataFrame(list(zip(titles, companies, locations, links)), columns = columns)
+    return results_dataframe
 
-def main():
-    soup = set_up()
-    df = get_info(soup)
-    df.to_csv('results.csv', index = False)
-    send_mail()
 
-def send_mail():
+def send_mail(email):
     port = 465
     
     #base message
@@ -89,8 +89,8 @@ def send_mail():
     
     #create a multipart message
     message = MIMEMultipart()
-    message["From"] = sender_email
-    message["To"] = sender_email
+    message["From"] = email
+    message["To"] = email
     message["Subject"] = subject
     message.attach(MIMEText(body, "plain"))
 
@@ -118,15 +118,22 @@ def send_mail():
         while True:
             password = getpass.getpass()
             try:
-                server.login(sender_email, password)
+                server.login(email, password)
             except smtplib.SMTPAuthenticationError:
                 print("If your gmail account is set up with 2-step verification, please enter your app password instead: ")
                 continue
             else:
-               server.sendmail(sender_email, receiver_email, text)
+               server.sendmail(email, text)
                print("Email has been sent")
                break 
 
-        
-# schedule.every(1).minutes.do(main)
+
+def main():
+    job, city, state, email = get_args()
+    soup = set_up(job, city, state)
+    results_dataframe = get_info(soup)
+    results_dataframe.to_csv('results.csv', index = False)
+    send_mail(email)
+
+
 main()
